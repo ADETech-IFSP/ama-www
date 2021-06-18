7cfe6836ab3971fc98a48b0ae1a4b1b0abc28234
adryanfederal <75398317+adryanfederal@users.noreply.github.com>
2021-06-18 10:29:29 -0300

    Link de redirecionamento
    Alterado o link de redirecionamento do pdf para o blog.

--- pages/pdf.ts
+++ pages/pdf.ts
@@ -1,3 +1,3 @@
 import redirect from 'nextjs-redirect'
 
-export default redirect('https://drive.google.com/file/d/1I2udhprkGzvJx3q1jETMuGmw85CrRaPe/view?usp=sharing', { statusCode: 302 })
+export default redirect('https://adesource.wordpress.com/', { statusCode: 302 })
