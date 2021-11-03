0086ba0eebc3ff51d6988a6f9dbcd1bb6ddf0a1d
Adryan Alencar <adryan.alencar@sevenfox.com.br>
2021-11-03 19:25:16 -0300

    Adicionado pagina para confirmar conta

---
+++ pages/confirm/[user].js
@@ -0,0 +1,34 @@
+
+export default function confirm() {
+    const router = useRouter();
+    const { id, code } = router.query;
+
+    const request = await fetch(`https://api.amemais.tech/user/account/confirm`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept-Content': 'application/json'
+        },
+        body: JSON.stringify({
+            id,
+            confirm_code: code
+        })
+    });
+
+    const response = await request.json();
+
+    if(response.status === 'success') {
+        return(
+            <div>
+                <h1>A conta foi ativada com sucesso.</h1>
+            </div>
+        )
+    }
+
+    return(
+        <div>
+            <h1>A conta não foi ativada.</h1>
+        </div>
+    )
+
+}
